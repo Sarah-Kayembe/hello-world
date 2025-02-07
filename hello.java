5bc783afa3b3f8cca02539bc3d469250612d64da
Sarah Kayembe <sarahkayembe@host-0800-3c22fb0faa47.pool.net.maine.edu>
2025-02-07 15:41:06 -0500

    Added the new message

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Here ye: important announcement");
+        System.out.println("Hello World!");
+        System.out.println("Goodbye and Farewell!");
     }
 }
